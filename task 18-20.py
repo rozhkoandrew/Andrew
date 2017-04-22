08f7ca360197411c8a12d4dc0ff4ccd1872b5d1d
rozhkoandrew <2c:69:58:4e:9f:c1:03:6d:2f:a7:97:27:91:38:bb:4b>
2017-04-22 14:08:59 +0300

    Howe_work

---
+++ task 18-20.py
@@ -0,0 +1,51 @@
+def unicode_symbl(unicode_1, unicode_2):
+    sum_of_unicode = 0
+    if ord(unicode_2) > ord(unicode_1):
+        for unicode_symbl in range (ord(unicode_1), ord(unicode_2)+1):
+            sum_of_unicode = sum_of_unicode + unicode_symbl
+        return sum_of_unicode
+    elif ord(unicode_1) > ord(unicode_2):
+        for unicode_symbl in range (ord(unicode_2), ord(unicode_1)+1):
+            sum_of_unicode = sum_of_unicode + unicode_symbl
+        return sum_of_unicode
+print(unicode_symbl('a','d'))
+
+
+def power_sum_counting(number):
+    x=0
+    summ_power = 0
+    while number**x<= 1000:
+        num = 3**x
+        summ_power = summ_power + num
+        x = x + 1
+    return summ_power
+print(power_sum_counting(3))
+
+
+def num_finder(*numbers):
+    for numbers in range(0,1001):
+        string = str(numbers)
+        if '1' in string and '7' in string:
+            print(numbers)
+num_finder(1,7)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
