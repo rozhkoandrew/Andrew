25924375e039243a6fc6c343ea1684122ee175bc
rozhkoandrew <2c:69:58:4e:9f:c1:03:6d:2f:a7:97:27:91:38:bb:4b>
2017-04-23 09:41:58 +0300

    Howe_work

--- task 18-20.py
+++ task 18-20.py
@@ -1,4 +1,4 @@
-def unicode_symbl(unicode_1, unicode_2):
+"""def unicode_symbl(unicode_1, unicode_2):
     sum_of_unicode = 0
     if ord(unicode_2) > ord(unicode_1):
         for unicode_symbl in range (ord(unicode_1), ord(unicode_2)+1):
@@ -8,7 +8,17 @@ def unicode_symbl(unicode_1, unicode_2):
         for unicode_symbl in range (ord(unicode_2), ord(unicode_1)+1):
             sum_of_unicode = sum_of_unicode + unicode_symbl
         return sum_of_unicode
+print(unicode_symbl('a','d')) """
+
+def unicode_symbl(unicode_1, unicode_2):
+    sum_of_unicode = 0
+    max_char = max(ord(unicode_1), ord(unicode_2))
+    min_char = min(ord(unicode_1), ord(unicode_2))
+    for char in range(min_char, max_char+1):
+        sum_of_unicode = sum_of_unicode + char
+    return sum_of_unicode
 print(unicode_symbl('a','d'))
+
 
 
 def power_sum_counting(number):
