76dacd7bfcc7b61cb09d8603f0a46ca7e5aa12ae
rozhkoandrew <2c:69:58:4e:9f:c1:03:6d:2f:a7:97:27:91:38:bb:4b>
2017-05-12 16:39:49 +0300

    Howe_work

--- Контрольная работа. 3е задание.py
+++ Контрольная работа. 3е задание.py
@@ -1,25 +1,17 @@
 import unittest
 import random
-
-
 def create_fibinachi_lst(amount_of_numbers):
     fibonachi_lst = [1, 1, 2]
     for x in range(3, amount_of_numbers):
         fibonachi_lst.append(fibonachi_lst[x - 1] + fibonachi_lst[x - 2])
     return fibonachi_lst
-
-
 def find_sum_fibonachi_list(amount_of_numbers):
     fibonachi_list = create_fibinachi_lst(amount_of_numbers)
     return sum(fibonachi_list)
-
-
 class TestFibonachiList(unittest.TestCase):
     def test_isfibonachi_numbers_inside(self):
         sum_fibonachi = find_sum_fibonachi_list(10)
         self.assertEquals = (sum_fibonachi, 143)
-
-
 def change_max_and_min(lst):
     max_value = max(lst)
     min_value = min(lst)
@@ -31,8 +23,6 @@ def change_max_and_min(lst):
             lst[x] = max_value
             continue
     return lst
-
-
 class TestisChangeMaxandMin(unittest.TestCase):
     def test_is_reverse_max_min(self):
         lst = change_max_and_min(create_fibinachi_lst(10))
@@ -40,11 +30,29 @@ class TestisChangeMaxandMin(unittest.TestCase):
         min_value_index = lst.index(min(lst))
         self.assertTrue(max_value_index == 0)
         self.assertTrue(min_value_index == 9)
-
-
+def normalize_lst(lst):
+    max_value = 0
+    persent_list = []
+    for x in range(0,len(lst)):
+        if abs(lst[x]) > max_value:
+            max_value = abs(lst[x])
+    for x in range(0, len(lst)):
+        persent_list.append(lst[x]/max_value)
+    return persent_list
+class TestIsNormilizeTo1(unittest.TestCase):
+    def test_is_normilize_to_1(self):
+        check_list = []
+        lst = [1, 3, 4, 5, 6, -4, 0, -0, -3]
+        for x in normalize_lst(lst):
+            if x >1 or x < -1 :
+                break
+            else:
+                check_list.append(x)
+        self.assertEqual(len(check_list) , len(lst))
 if __name__ == "__main__":
     print(find_sum_fibonachi_list(10))
     print(change_max_and_min(create_fibinachi_lst(10)))
+    print(normalize_lst([x for x in range(-5, 6)]))
     unittest.main()
 
 
