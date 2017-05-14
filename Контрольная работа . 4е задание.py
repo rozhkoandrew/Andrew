8fc69feb514cb2fbd1b997d193f6fee893308c13
rozhkoandrew <2c:69:58:4e:9f:c1:03:6d:2f:a7:97:27:91:38:bb:4b>
2017-05-14 14:19:23 +0300

    Howe_work

--- Контрольная работа . 4е задание.py
+++ Контрольная работа . 4е задание.py
@@ -1,9 +1,7 @@
 import random
-
+import unittest
 first_lst = [x for x in range(2,10)]
 second_lst = [x for x in range(2,10)]
-final_lst = []
-
 def make_multily(number_1,number_2):
     mult = ('%s * %s'%(number_2,number_1))
     return mult
@@ -23,7 +21,25 @@ def take_numbers_from_list(first_lst,second_lst):
 
         x+=1
     return lst_1
-print(take_numbers_from_list(first_lst,second_lst))
-#
+
+class TestMultiplyExamples(unittest.TestCase):
+    def test_is_not_repeats_itself(self):
+        check_lst = []
+        first_lst = [x for x in range(2, 10)]
+        second_lst = [x for x in range(2, 10)]
+        result = take_numbers_from_list(first_lst,second_lst)
+        for x in result:
+            if x in check_lst:
+                break
+            elif x[::-1] in check_lst:
+                break
+            else:
+                check_lst.append(x)
+        self.assertTrue(len(check_lst) == 15)
+ #print(x[::-1],x)
+
+if __name__ =='__main__':
+    print(take_numbers_from_list(first_lst, second_lst))
+    unittest.main()
 
 
