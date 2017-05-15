e70de37ecfa6210c0dd472dc9eee025a90895464
rozhkoandrew <2c:69:58:4e:9f:c1:03:6d:2f:a7:97:27:91:38:bb:4b>
2017-05-15 16:39:21 +0300

    Howe_work

--- Контрольная работа . 4е задание.py
+++ Контрольная работа . 4е задание.py
@@ -2,14 +2,14 @@ import random
 import unittest
 first_lst = [x for x in range(2,10)]
 second_lst = [x for x in range(2,10)]
+amount_of_multipliers = 15
 def make_multily(number_1,number_2):
     mult = ('%s * %s'%(number_2,number_1))
     return mult
-
 def take_numbers_from_list(first_lst,second_lst):
     lst_1 = []
     x = 0
-    while x !=15:
+    while x !=amount_of_multipliers:
         a = random.choice(first_lst)
         b = random.choice(second_lst)
         if make_multily(a,b) in lst_1:
@@ -21,7 +21,6 @@ def take_numbers_from_list(first_lst,second_lst):
 
         x+=1
     return lst_1
-
 class TestMultiplyExamples(unittest.TestCase):
     def test_is_not_repeats_itself(self):
         check_lst = []
@@ -34,9 +33,9 @@ class TestMultiplyExamples(unittest.TestCase):
             elif x[::-1] in check_lst:
                 break
             else:
+                # print(x[::-1],x)
                 check_lst.append(x)
-        self.assertTrue(len(check_lst) == 15)
- #print(x[::-1],x)
+        self.assertTrue(len(check_lst) == amount_of_multipliers)
 
 if __name__ =='__main__':
     print(take_numbers_from_list(first_lst, second_lst))
